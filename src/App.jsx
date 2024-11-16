8b951794d4839f7b994750b510d82d66399569f1
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-11-17 03:09:10 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+    return (
+        <main className='min-h-screen text-slate-900 bg-[#F7F7F7]'>
+            <Navbar />
+        </main>
+    );
+}
+
+export default App;
