fe01161d2acc470cd65673a15fafb42ca961c84a
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-11-17 03:12:50 +0600

    add: created Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,3 +1,12 @@
 export default function Navbar() {
-    return <section></section>;
+    return (
+        <section className='flex justify-between px-8 py-5'>
+            <h2 className='text-2xl font-semibold font-mono'>Pictura</h2>
+
+            <ul className='flex items-center gap-x-5'>
+                <li>Saved</li>
+                <button>Login</button>
+            </ul>
+        </section>
+    );
 }
