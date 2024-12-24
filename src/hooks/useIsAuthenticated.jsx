aed10e4019e77c3db6809e86a2d4cbb8133dc5b7
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-12-24 09:18:10 +0600

    add: created useIsAuthenticated custom hook to check auth state

---
+++ src/hooks/useIsAuthenticated.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect, useContext } from 'react';
+import AuthContext from '../contexts/authContext';
+import UserContext from '../contexts/UserContext';
+
+export default function useIsAuthenticated() {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    const { setAuthToken } = useContext(AuthContext);
+    const { setUser } = useContext(UserContext);
+
+    // Check if the user is authenticated
+    useEffect(() => {
+        const existingCred = JSON.parse(localStorage.getItem('auth'));
+
+
+        
+        if (existingCred.accessToken && existingCred.user.id) {
+            // Set the auth token and user
+            setAuthToken({ token: existingCred.accessToken });
+            setUser({ data: existingCred.user });
+        }
+
+        setIsAuthenticated(true);
+    }, [setAuthToken, setUser]);
+
+    return isAuthenticated;
+}
