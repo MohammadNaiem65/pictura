86186ac1f8cc47faf7db072003b766de5e1d8928
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-12-25 10:55:49 +0600

    fix: fixed AuthContext path typo

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { AuthContextProvider } from './contexts/authContext.jsx';
+import { AuthContextProvider } from './contexts/AuthContext.jsx';
 import { UserContextProvider } from './contexts/UserContext.jsx';
 import router from './router/router.jsx';
 import './index.css';
