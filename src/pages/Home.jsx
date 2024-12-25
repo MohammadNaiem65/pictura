14c2ef9e32416c252a5606fefdc378954044c038
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-12-25 17:06:02 +0600

    add: completed Home page design

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Banner from '../components/Home/Banner';
+import ProductGrid from '../components/Home/ProductsGrid';
+import Contact from '../components/Home/Contact';
+
+export default function Home() {
+    return (
+        <>
+            <Banner />
+            <ProductGrid />
+            <Contact />
+        </>
+    );
+}
