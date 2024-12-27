ce5ec0f658f4aa17a8c30a4198e7b0454ec7f603
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-12-27 14:59:59 +0600

    update: made the CartSummary component functional

--- src/components/Cart/CartSummary.jsx
+++ src/components/Cart/CartSummary.jsx
@@ -1,5 +1,27 @@
+import { useState, useContext } from 'react';
+import CartContext from '../../contexts/CartContext';
+import toast from 'react-hot-toast';
+import LoadingSpinner from '../shared/LoadingSpinner';
+
 export default function CartSummary({ subtotal, shipping }) {
-    return (
+    const [showLoader, setShowLoader] = useState(false);
+    const { setCart } = useContext(CartContext);
+
+    const handleConfirmOrder = () => {
+        setShowLoader(true);
+
+        setTimeout(() => {
+            setShowLoader(false);
+            localStorage.removeItem('cart');
+            setCart([]);
+
+            toast.success('Order confirmed! 🎉');
+        }, 2200);
+    };
+
+    return showLoader ? (
+        <LoadingSpinner />
+    ) : (
         <div className='lg:col-span-1'>
             <div className='bg-gray-50 p-6 rounded-lg'>
                 <h3 className='text-xl font-semibold mb-4'>Order Summary</h3>
@@ -21,8 +43,11 @@ export default function CartSummary({ subtotal, shipping }) {
                         </div>
                     </div>
                 </div>
-                <button className='w-full bg-indigo-600 text-white py-2 px-4 rounded-md mt-6 hover:bg-indigo-700 transition-colors'>
-                    Proceed to Checkout
+                <button
+                    onClick={handleConfirmOrder}
+                    className='w-full bg-indigo-600 text-white py-2 px-4 rounded-md mt-6 hover:bg-indigo-700 transition-colors'
+                >
+                    Order
                 </button>
             </div>
         </div>
